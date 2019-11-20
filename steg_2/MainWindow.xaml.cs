06af76a31452361acd1011b2209326239e466538
Damian Jozwiak <damian.jozwiak@synapse.com>
2019-11-20 22:27:33 +0100

    finished

---
+++ steg_2/MainWindow.xaml.cs
@@ -0,0 +1,72 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Drawing;
+using System.Drawing.Imaging;
+using steg_2.lib;
+
+namespace steg_2
+{
+    /// <summary>
+    /// Logika interakcji dla klasy MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        Bitmap source;
+        Bitmap destination;
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        private void load_image_button_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            if (openFileDialog.ShowDialog() == true)
+            {
+
+                source = new Bitmap(openFileDialog.FileName);
+                source_image.Source = System.Windows.Interop.Imaging.CreateBitmapSourceFromHBitmap(
+                                           source.GetHbitmap(),
+                                           IntPtr.Zero,
+                                           System.Windows.Int32Rect.Empty,
+                                           BitmapSizeOptions.FromWidthAndHeight(source.Width, source.Height));
+            }
+        }
+
+        private void save_image_button_Click(object sender, RoutedEventArgs e)
+        {
+            SaveFileDialog dialog = new SaveFileDialog();
+            if (dialog.ShowDialog() == true)
+            {
+                destination.Save(dialog.FileName, ImageFormat.Bmp);
+            }
+        }
+
+
+        private void noise_button_Click(object sender, RoutedEventArgs e)
+        {
+            BmpTransform bt = new BmpTransform(source);
+            var nb = bt.markedBitmap();
+            destination_image.Source = System.Windows.Interop.Imaging.CreateBitmapSourceFromHBitmap(
+                                           nb.GetHbitmap(),
+                                           IntPtr.Zero,
+                                           System.Windows.Int32Rect.Empty,
+                                           BitmapSizeOptions.FromWidthAndHeight(nb.Width, nb.Height));
+            var total =  bt.nrOFPixelsChanged()*100/(nb.Width*nb.Height);
+            cLabel.Content = total.ToString() + " %";
+        }
+    }
+}
---
+++ steg_2/lib/BmpTransform.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace steg_2.lib
+{
+    
+    class BmpTransform
+    {
+        Bitmap _newBitmap;
+        Bitmap _oldBitmap;
+        public BmpTransform(Bitmap source)
+        {
+            MemoryStream memoryStream = new MemoryStream();
+            _oldBitmap = source;
+            source.Save(memoryStream, System.Drawing.Imaging.ImageFormat.Jpeg);
+            _newBitmap = new Bitmap(memoryStream);
+        }
+        public Bitmap markedBitmap()
+        {
+            Bitmap result = new Bitmap(_oldBitmap);
+            var list = changedPixels();
+            Color nc = Color.Red;
+            for (int i =0; i< list.Count; i++)
+            {
+                result.SetPixel(list[i].X, list[i].Y, nc);
+            }
+            return result;
+        }
+        public List<Point> changedPixels()
+        {
+            List<Point> changed = new List<Point>();
+            for (int x = 0; x < _oldBitmap.Width; x++)
+            {
+                for (int y = 0; y < _oldBitmap.Height; y++)
+                {
+                    Color oldclr = _oldBitmap.GetPixel(x, y);
+                    Color newclr = _newBitmap.GetPixel(x, y);
+                    if(oldclr != newclr)
+                    {
+                        changed.Add(new Point(x, y));
+                    }
+                }
+            }
+            return changed;
+        }
+        public int nrOFPixelsChanged()
+        {
+            int changed = 0;
+            for (int x = 0; x < _oldBitmap.Width; x++)
+            {
+                for (int y = 0; y < _oldBitmap.Height; y++)
+                {
+                    Color oldclr = _oldBitmap.GetPixel(x, y);
+                    Color newclr = _newBitmap.GetPixel(x, y);
+                    if (oldclr != newclr)
+                    {
+                        changed++;
+                    }
+                }
+            }
+            return changed;
+        }
+    }
+}
